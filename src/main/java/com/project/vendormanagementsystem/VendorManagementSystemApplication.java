c52b5d186e551e0d87b6c5c7361d6f9f03e15ec7
Deep Sanghani <deepsanghani3@gmil.com>
2024-05-05 17:10:00 +0530

    changes

---
+++ src/main/java/com/project/vendormanagementsystem/VendorManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.vendormanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendorManagementSystemApplication.class, args);
+	}
+
+}
