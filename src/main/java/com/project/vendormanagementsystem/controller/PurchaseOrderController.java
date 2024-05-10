fa3ad4d2810c05b1948f1603672edb2ad5688fa0
Deep Sanghani <deepsanghani3@gmil.com>
2024-05-10 09:15:05 +0530

    purchase order getall, get with id and post api added

--- src/main/java/com/project/vendormanagementsystem/controller/PurchaseOrderController.java
+++ src/main/java/com/project/vendormanagementsystem/controller/PurchaseOrderController.java
@@ -1,7 +1,39 @@
 package com.project.vendormanagementsystem.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.project.vendormanagementsystem.entity.PurchaseOrder;
+import com.project.vendormanagementsystem.service.PurchaseOrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
+@Slf4j
 public class PurchaseOrderController {
+    @Autowired
+    private PurchaseOrderService purchaseOrderService;
+
+    @PostMapping("api/purchase_orders/")
+    public ResponseEntity<?> addPurchaseOrder(@RequestBody PurchaseOrder purchaseOrder){
+        purchaseOrderService.addPurchaseOrder(purchaseOrder);
+        return new ResponseEntity<>("Purhcase Order Added Successfully", HttpStatus.OK);
+    }
+
+    @GetMapping("api/purchase_orders/")
+    public ResponseEntity<?> fetchAllPucharseOrder(){
+        return new ResponseEntity<>(purchaseOrderService.getAllPurchaseOrder(), HttpStatus.OK);
+    }
+
+    @GetMapping("api/purchase_orders/{po_id}")
+    public ResponseEntity<?> fetchPucharseOrder(@PathVariable String po_id) {
+        Optional<PurchaseOrder> purchaseOrder = purchaseOrderService.getPurchaseOrderById(po_id);
+        if (purchaseOrder.isPresent()) {
+            return new ResponseEntity<>(purchaseOrder.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Not found purchase Order", HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- src/main/java/com/project/vendormanagementsystem/service/PurchaseOrderService.java
+++ src/main/java/com/project/vendormanagementsystem/service/PurchaseOrderService.java
@@ -1,7 +1,27 @@
 package com.project.vendormanagementsystem.service;
 
+import com.project.vendormanagementsystem.entity.PurchaseOrder;
+import com.project.vendormanagementsystem.repository.PurchaseOrderRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PurchaseOrderService {
+    @Autowired
+    private PurchaseOrderRepo purchaseOrderRepo;
+    public boolean addPurchaseOrder(PurchaseOrder purchaseOrder){
+        purchaseOrderRepo.save(purchaseOrder);
+        return true;
+    }
+
+    public List<PurchaseOrder> getAllPurchaseOrder(){
+        return purchaseOrderRepo.findAll();
+    }
+
+    public Optional<PurchaseOrder> getPurchaseOrderById(String id){
+        return purchaseOrderRepo.findById(id);
+    }
 }
